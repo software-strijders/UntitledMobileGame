8e7b659f5c1abc23809aeb462a6f8471ea98791d
Xander Vedder <xander.vedd@gmail.com>
2022-08-27 11:14:13 +0200

    Add namespace to `Skipper.cs`

---
+++ Scripts/Skipper.cs
@@ -0,0 +1,17 @@
+using Godot;
+
+namespace UntitledMobileGame.Scripts
+{
+    public class Skipper : KinematicBody
+    {
+        [Export] public int Gravity = 10;
+    
+        private Vector3 _velocity = Vector3.Zero;
+
+        public override void _PhysicsProcess(float delta)
+        {
+            _velocity.y -= Gravity;
+            _velocity = MoveAndSlide(_velocity, Vector3.Up);
+        }
+    }
+}
